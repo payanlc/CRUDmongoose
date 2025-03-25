9f11bb7ca7f7d8bf34d5d6929d6429aefa970ad4
Lauro Carlos Payán Reyes <payanlc@yahoo.com.mx>
2025-03-24 20:45:45 -0600

    poniendo variable de entorno

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("view engine","ejs")
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
 
-mongoose.connect(MIMONGO)
+mongoose.connect(process.env.MIMONGO)
 .then (()=>{
     console.log("Conectado exitosamente");
 })
