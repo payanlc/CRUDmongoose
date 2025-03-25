33a957900c8a58a73de60966744497a6ebfd7e63
Lauro Carlos Payán Reyes <payanlc@yahoo.com.mx>
2025-03-24 18:55:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("view engine","ejs")
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
 
-mongoose.connect("mongodb+srv://admin:5tzwOmi8FGUwj9uR@backenddb.sut3l.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+mongoose.connect(MIMONGO)
 .then (()=>{
     console.log("Conectado exitosamente");
 })
